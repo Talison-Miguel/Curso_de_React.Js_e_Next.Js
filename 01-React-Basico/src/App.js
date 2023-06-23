ab142535d0cb28724def8ca5b79e862bf3f11e69
Tálison Miguel <talisonmiguel84@gmail.com>
2023-06-22 23:09:39 -0300

    npx create

--- 01-React-Basico/src/App.js
+++ 01-React-Basico/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Primeiro projeto do curso de React.Js
         </p>
         <a
           className="App-link"
