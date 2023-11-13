d451e1dd488d9f73b13a4b7481ac6ee8c762bb65
Tálison Miguel <talisonmiguel84@gmail.com>
2023-11-13 15:32:31 -0300

    useRef

--- 02-React Hooks/react-hooks-2/src/App.js
+++ 02-React Hooks/react-hooks-2/src/App.js
@@ -1,13 +1,15 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import './App.css';
 
 import P from 'prop-types';
 
-const Post = ({ post }) => {
+const Post = ({ post, handleClick }) => {
     console.log('Filho renderizou');
     return (
         <div key={post.id} className="post">
-            <h1>{post.title}</h1>
+            <h1 style={{ fontSize: '14px' }} onClick={() => handleClick(post.title)}>
+                {post.title}
+            </h1>
             <p>{post.body}</p>
         </div>
     );
@@ -19,34 +21,49 @@ Post.propTypes = {
         title: P.string,
         body: P.string,
     }),
+    handleClick: P.func,
 };
 
 function App() {
     const [posts, setPosts] = useState([]);
     const [value, setValue] = useState('');
+    const input = useRef(null);
+    const contador = useRef(0);
 
     console.log('Pai renderizou!');
 
     //Component did mount
     useEffect(() => {
-        setTimeout(function () {
-            fetch('https://jsonplaceholder.typicode.com/posts')
-                .then((response) => response.json())
-                .then((response) => setPosts(response));
-        }, 5000);
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then((response) => response.json())
+            .then((response) => setPosts(response));
     }, []);
+
+    useEffect(() => {
+        input.current.focus();
+        console.log(input.current);
+    }, [value]);
+
+    useEffect(() => {
+        contador.current++;
+    }, [value]);
+
+    const handleClick = (value) => {
+        setValue(value);
+    };
 
     return (
         <div className="App">
             <p>
-                <input type="search" value={value} onChange={(e) => setValue(e.target.value)} />
+                <input ref={input} type="search" value={value} onChange={(e) => setValue(e.target.value)} />
             </p>
+            <h3>Renderizou: {contador.current}</h3>
 
             {useMemo(() => {
                 return (
                     posts.length > 0 &&
                     posts.map((post) => {
-                        return <Post key={post.id} post={post} />;
+                        return <Post key={post.id} post={post} handleClick={handleClick} />;
                     })
                 );
             }, [posts])}
