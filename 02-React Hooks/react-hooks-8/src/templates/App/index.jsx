67f7ff9cbb4b6ccb18038be4477e9d330c38c62d
Tálison Miguel <talisonmiguel84@gmail.com>
2023-12-18 15:07:02 -0300

    custom hook

--- 02-React Hooks/react-hooks-8/src/templates/App/index.jsx
+++ 02-React Hooks/react-hooks-8/src/templates/App/index.jsx
@@ -1,44 +1,39 @@
-import { useEffect } from 'react';
 import { useState } from 'react';
+import { useFetch } from './use-fetch';
 
-const useFetch = (url, options) => {
-    const [result, setResult] = useState(null);
-    const [loading, setLoading] = useState(false);
-
-    useEffect(() => {
-        console.log('Effect', new Date().toLocaleString());
-        setLoading(true);
-
-        const fetchData = async () => {
-            // eslint-disable-next-line no-useless-catch
-            try {
-                await new Promise((r) => setTimeout(r, 3000));
-                const response = await fetch(url, options);
-                const jsonResult = await response.json();
-
-                setResult(jsonResult);
-                setLoading(false);
-            } catch (e) {
-                setLoading(false);
-                throw e;
-            }
-        };
-
-        fetchData();
-    }, [url, options]);
-
-    return [result, loading];
-};
-
-const Home = () => {
-    const [result, loading] = useFetch('https://jsonplaceholder.typicode.com/posts');
+export const Home = () => {
+    const [postId, setPostId] = useState('');
+    const [result, loading] = useFetch('https://jsonplaceholder.typicode.com/posts/' + postId, {
+        headers: {
+            abc: '1' + postId,
+        },
+    });
 
     if (loading) {
         return <p>Loading...</p>;
     }
 
+    const handleClick = (id) => {
+        setPostId(id);
+    };
+
     if (!loading && result) {
-        console.log(result);
+        // 1
+        return (
+            <div>
+                {result?.length > 0 ? (
+                    result.map((p) => (
+                        <div key={`post-${p.id}`} onClick={() => handleClick(p.id)}>
+                            <p>{p.title}</p>
+                        </div>
+                    ))
+                ) : (
+                    <div onClick={() => handleClick('')}>
+                        <p>{result.title}</p>
+                    </div>
+                )}
+            </div>
+        );
     }
 
     return <h1>Oi</h1>;
