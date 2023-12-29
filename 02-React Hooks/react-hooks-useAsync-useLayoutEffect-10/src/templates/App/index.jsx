b077c5f9890d77e852d987e0d805740fc87cab8a
Tálison Miguel <talisonmiguel84@gmail.com>
2023-12-29 13:30:18 -0300

    useImperativeHandle

--- 02-React Hooks/react-hooks-useAsync-useLayoutEffect-10/src/templates/App/index.jsx
+++ 02-React Hooks/react-hooks-useAsync-useLayoutEffect-10/src/templates/App/index.jsx
@@ -1,29 +1,54 @@
-import { useCallback, useEffect, useLayoutEffect, useRef, useState } from 'react';
+/* eslint-disable react/prop-types */
+import { forwardRef, useCallback, useEffect, useImperativeHandle, useLayoutEffect, useRef, useState } from 'react';
 
 export const Home = () => {
     const [counted, setCounted] = useState([0, 1, 2, 3]);
 
     const handleClick = () => {
         setCounted((c) => [...c, +c.slice(-1) + 1]);
+        divRef.current.handleClick();
     };
 
     const divRef = useRef();
 
     useLayoutEffect(() => {
         const now = Date.now();
-        while (Date.now() < now + 600) divRef.current.scrollTop = divRef.current.scrollHeight;
+        while (Date.now() < now + 300) divRef.current.divRef.scrollTop = divRef.current.divRef.scrollHeight;
     });
 
     return (
         <>
             <button onClick={handleClick}>Count {counted.slice(-1)}</button>
-            <div ref={divRef} style={{ height: '100px', width: '100px', overflow: 'scroll', padding: '10px' }}>
-                {counted.map((c) => {
-                    return <p key={`c-${c}`}>{c}</p>;
-                })}
-            </div>
+            <DisplayCounted counted={counted} ref={divRef}></DisplayCounted>
         </>
     );
 };
+
+// eslint-disable-next-line react/prop-types
+export const DisplayCounted = forwardRef(function DisplayCounted({ counted }, ref) {
+    const [rand, setRand] = useState('0.24');
+    const divRef = useRef();
+
+    const handleClick = () => {
+        setRand(Math.random().toFixed(2));
+    };
+
+    useImperativeHandle(ref, () => ({
+        handleClick,
+        divRef: divRef.current,
+    }));
+
+    return (
+        <div ref={divRef} style={{ height: '100px', width: '100px', overflow: 'scroll', padding: '10px' }}>
+            {counted.map((c) => {
+                return (
+                    <p onClick={handleClick} key={`c-${c}`}>
+                        {c} +++ {rand}
+                    </p>
+                );
+            })}
+        </div>
+    );
+});
 
 export default Home;
