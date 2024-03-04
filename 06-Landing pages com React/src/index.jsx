b709499dfbc250e1306bee24348d7697a52dbfe0
Tálison Miguel <talisonmiguel84@gmail.com>
2024-03-04 12:12:12 -0300

    styled-components

--- 06-Landing pages com React/src/index.jsx
+++ 06-Landing pages com React/src/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from './styles/global-styles';
+
+import { theme } from './styles/theme';
 import Home from './templates/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Home />
+        <ThemeProvider theme={theme}>
+            <Home />
+            <GlobalStyles />
+        </ThemeProvider>
     </React.StrictMode>,
 );
